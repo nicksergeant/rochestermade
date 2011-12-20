a6089f8cd03d3ab1061b9d77eb1f476aba600e82
Nick Sergeant <nick@nicksergeant.com>
2011-12-19 20:39:32 -0500

    Adding dialog and support for slide sticking

--- public/media/js/script.js
+++ public/media/js/script.js
@@ -1,14 +1,57 @@
 $(function() {
-    setTimeout(function() {
-        rotate();
-    }, 6000);
+
+    var slide = getParameterByName('slide');
+
+    if (slide !== '') {
+        if ($('div.' + slide).length) {
+            $('div.current').removeClass('current');
+            $('div.' + slide).addClass('current');
+            $('div.container').attr('id', slide);
+        } else {
+            init();
+        }
+    } else {
+        init();
+    }
+
+    $awesome = $('div.awesome');
 
     $('a#logo').click(function() {
-        //$('div.awesome').show();
+        $awesome.fadeToggle(100);
+        return false;
+    });
+
+    $('a.close', $awesome).click(function() {
+        $awesome.fadeOut(100);
+        return false;
+    });
+
+    $('div.choose a', $awesome).click(function() {
+        $('textarea', $awesome).hide();
+        $('textarea.' + $(this).attr('class')).show();
+        return false;
+    });
+
+    $awesome.click(function(event){
+        event.stopPropagation();
+    });
+
+    $('textarea', $awesome).click(function() {
+        $(this).select();
+        return false;
+    });
+
+    $('html').click(function() {
+        $awesome.hide();
         return false;
     });
 });
 
+function init() {
+    setTimeout(function() {
+        rotate();
+    }, 6000);
+}
 function rotate() {
 
     var $container = $('div.container');
@@ -30,4 +73,16 @@ function rotate() {
     setTimeout(function() {
         rotate();
     }, 6000);
+}
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.href);
+    if (results === null) {
+        return "";
+    }
+    else {
+        return decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
 }
